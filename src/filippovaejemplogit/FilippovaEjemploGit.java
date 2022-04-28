3f1d39684cce5775fafd2289c02d177e87139ab8
User <User@LAPTOP-5CM3L1HQ>
2022-04-28 18:38:16 +0200

    versión 1

---
+++ src/filippovaejemplogit/FilippovaEjemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filippovaejemplogit;
+
+/**
+ *
+ * @author User
+ */
+public class FilippovaEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Gina");
+        System.out.println("Versión 1");
+    }
+    
+}
