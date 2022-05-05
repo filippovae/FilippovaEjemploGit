8388db3862b84d40127b76cc7f9bce05ffd18b19
damt130 <damt130@36-31.aglinformatica.es>
2022-05-05 17:17:17 +0200

    versión Pedro

--- src/filippovaejemplogit/FilippovaEjemploGit.java
+++ src/filippovaejemplogit/FilippovaEjemploGit.java
@@ -15,7 +15,7 @@ public class FilippovaEjemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Gina");
+        System.out.println("Gina y Pedro");
         System.out.println("Versión 2");
     }
     
